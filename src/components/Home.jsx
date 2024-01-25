946967f1c3e9bda70a59aa87de39bf1d1dc05500
anasnadkar45 <anasnadkar23@gmail.com>
2024-01-25 22:17:49 +0530

    Adding files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import 
-
+import herobg from '../assets/herobg.png'
+import author from '../assets/author.png'
 function Home() {
   return (
-    <div className=''>
-      <img src="" alt="" />
+    <div className="bg-cover bg-center h-screen" style={{ backgroundImage: `url(${herobg})` }}>
+      <div className="max-w-[1200px] m-auto flex justify-between items-center">
+        <h1>Anas Nadkar</h1>
+        <img src={author} alt="" className='w-[500px]'/>
+      </div>
     </div>
   )
 }
